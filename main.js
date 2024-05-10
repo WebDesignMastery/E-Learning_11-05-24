33089065f1f8f9eee3293faf8a7780ccc2eb303c
Web Design Mastery <130627020+WebDesignMastery@users.noreply.github.com>
2024-05-11 00:05:07 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__content form", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
